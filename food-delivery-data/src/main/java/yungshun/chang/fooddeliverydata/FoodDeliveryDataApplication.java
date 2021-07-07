450345940ecc866c64012e15652dfd0921cfbe8c
yungshun317 <seraph317@gmail.com>
2021-07-07 19:32:36 +0800

    [CHORE] Create the `food-delivery-data` module for placing JPA repositories

---
+++ food-delivery-data/src/main/java/yungshun/chang/fooddeliverydata/FoodDeliveryDataApplication.java
@@ -0,0 +1,13 @@
+package yungshun.chang.fooddeliverydata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodDeliveryDataApplication.class, args);
+    }
+
+}
---
+++ food-delivery-data/src/test/java/yungshun/chang/fooddeliverydata/FoodDeliveryDataApplicationTests.java
@@ -0,0 +1,13 @@
+package yungshun.chang.fooddeliverydata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

0498b87fff32c405ae8f4aeccdcb8a0c03b5bdda
yungshun317 <seraph317@gmail.com>
2021-07-07 19:28:48 +0800

    [CHORE] Create the `food-delivery-domain` module for defining models

---
+++ food-delivery-domain/src/main/java/yungshun/chang/fooddeliverydomain/FoodDeliveryDomainApplication.java
@@ -0,0 +1,13 @@
+package yungshun.chang.fooddeliverydomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryDomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodDeliveryDomainApplication.class, args);
+    }
+
+}

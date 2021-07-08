f066997110ecbb4df8fdc28d37d9f83b438da5b3
yungshun317 <seraph317@gmail.com>
2021-07-08 11:20:42 +0800

    [CHORE] Create the `food-delivery-rest-client` module for implementing REST client

---
+++ food-delivery-rest-client/src/main/java/yungshun/chang/fooddeliveryrestclient/FoodDeliveryRestClientApplication.java
@@ -0,0 +1,13 @@
+package yungshun.chang.fooddeliveryrestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryRestClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodDeliveryRestClientApplication.class, args);
+    }
+
+}
---
+++ food-delivery-rest-client/src/test/java/yungshun/chang/fooddeliveryrestclient/FoodDeliveryRestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package yungshun.chang.fooddeliveryrestclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryRestClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

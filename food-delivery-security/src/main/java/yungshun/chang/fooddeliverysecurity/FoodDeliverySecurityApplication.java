628685fc0458b0132b32fd7cd0bbd65c8d477fe7
yungshun317 <seraph317@gmail.com>
2021-07-08 11:17:12 +0800

    [CHORE] Create the `food-delivery-security` module for handling registration

---
+++ food-delivery-security/src/main/java/yungshun/chang/fooddeliverysecurity/FoodDeliverySecurityApplication.java
@@ -0,0 +1,13 @@
+package yungshun.chang.fooddeliverysecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliverySecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodDeliverySecurityApplication.class, args);
+    }
+
+}
---
+++ food-delivery-security/src/test/java/yungshun/chang/fooddeliverysecurity/FoodDeliverySecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package yungshun.chang.fooddeliverysecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliverySecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

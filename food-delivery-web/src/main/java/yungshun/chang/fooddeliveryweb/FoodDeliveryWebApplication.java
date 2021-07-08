1848dd6e1f708386a04c6ea81b8c5ebcaeb4bfda
yungshun317 <seraph317@gmail.com>
2021-07-08 11:01:23 +0800

    [CHORE] Create the `food-delivery-web` module for configuring Spring Web MVC

---
+++ food-delivery-web/src/main/java/yungshun/chang/fooddeliveryweb/FoodDeliveryWebApplication.java
@@ -0,0 +1,13 @@
+package yungshun.chang.fooddeliveryweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodDeliveryWebApplication.class, args);
+    }
+
+}
---
+++ food-delivery-web/src/test/java/yungshun/chang/fooddeliveryweb/FoodDeliveryWebApplicationTests.java
@@ -0,0 +1,13 @@
+package yungshun.chang.fooddeliveryweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

ea95257cb20be2a9f81beed3a68bec99b631d950
yungshun317 <seraph317@gmail.com>
2021-07-08 11:14:30 +0800

    [CHORE] Create the `food-delivery-ui` module for locating front end application

---
+++ food-delivery-ui/src/main/java/yungshun/chang/fooddeliveryui/FoodDeliveryUiApplication.java
@@ -0,0 +1,13 @@
+package yungshun.chang.fooddeliveryui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodDeliveryUiApplication.class, args);
+    }
+
+}
---
+++ food-delivery-ui/src/test/java/yungshun/chang/fooddeliveryui/FoodDeliveryUiApplicationTests.java
@@ -0,0 +1,13 @@
+package yungshun.chang.fooddeliveryui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

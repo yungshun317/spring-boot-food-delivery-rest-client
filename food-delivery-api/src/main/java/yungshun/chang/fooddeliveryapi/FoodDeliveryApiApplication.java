85a8aca0888b38d2d018453ce389e7052ef61205
yungshun317 <seraph317@gmail.com>
2021-07-08 11:27:25 +0800

    [CHORE] Create the `food-delivery-api` module for building REST services

---
+++ food-delivery-api/src/main/java/yungshun/chang/fooddeliveryapi/FoodDeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package yungshun.chang.fooddeliveryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodDeliveryApiApplication.class, args);
+    }
+
+}
---
+++ food-delivery-api/src/test/java/yungshun/chang/fooddeliveryapi/FoodDeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package yungshun.chang.fooddeliveryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
